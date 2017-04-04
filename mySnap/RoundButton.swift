37dfebfd16814d13646aef7247c2de2685a11765
Marvin Xie <marvinxie90@gmail.com>
2017-04-04 00:13:24 -0700

    customized loging page

---
+++ mySnap/RoundButton.swift
@@ -0,0 +1,37 @@
+//
+//  RoundButton.swift
+//  mySnap
+//
+//  Created by Joy on 4/4/17.
+//  Copyright © 2017 me. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+}
